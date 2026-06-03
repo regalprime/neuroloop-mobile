3375e9fdcd2cba5fdd54311ffe7df0f9f5b43376
CongKhanh <congkhanh.developer@gmail.com>
2026-06-03 15:34:23 +0700

    NeuroLoop app

---
+++ android/app/src/main/kotlin/com/neuroloop/neuroloop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.neuroloop.neuroloop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main/main_dev.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NeuroLoopApp());
+}
+
+class NeuroLoopApp extends StatelessWidget {
+  const NeuroLoopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
