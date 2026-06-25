9697775ed21fd0b64593e386683a9ed17becef3c
CongKhanh <congkhanh.developer@gmail.com>
2026-06-25 15:41:36 +0700

    feat: reader

--- lib/features/dashboard/presentation/dashboard_page.dart
+++ lib/features/dashboard/presentation/dashboard_page.dart
@@ -1,1 +1,25 @@
-...
+import 'package:flutter/material.dart';
+
+import 'package:neuroloop/domain/extension/app_extension.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final userProfile = Text(context.l10n.hello);
+    return Scaffold(
+      body: Column(
+        children: [
+          userProfile,
+          Container(
+            /// koking
+            color: Colors.red,
+            padding: const EdgeInsets.all(5),
+            margin: const EdgeInsets.all(5),
+          )
+        ],
+      ),
+    );
+  }
+}
